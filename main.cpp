a0b558bafe99dde0191579265711ab84b2654325
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-04-25 01:49:23 +0200

    more tests

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 
 int main(void) {
     poss::Option<int> some(420);
+    assert(some.is_some());
+
     poss::Option<int> none;
+    assert(none.is_none());
 
     assert(typeid(poss::Option<int>::type_value) == typeid(int));
 
